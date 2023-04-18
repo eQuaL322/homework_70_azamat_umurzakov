84e8293e08fe23853d6511437b5137e55bbada91
Azamat Umurzakov <equal32296@gmail.com>
2023-04-18 06:15:59 +0600

    Добавил ProjectView, TaskView и Serializers для Project, User, Status, Type, Task - Создал ProjectView и методы get, put, delete - Создал TaskView и методы get, put, delete - Добавил urls для этих методов - Создал serializers для Project, User, Status, Type, Task - Изменил README.md

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api.views import ProjectView, TaskView
 
 urlpatterns = [
     path('project/<int:pk>', ProjectView.as_view()),
-    path('project/', ProjectView.as_view()),
+    path('projects/', ProjectView.as_view()),
     path('task/<int:pk>', TaskView.as_view()),
-    path('task/', TaskView.as_view()),
+    path('tasks/', TaskView.as_view()),
 ]
